a1138a8e5c272c7587e3bfc55f972f89881ad99c
Saurabh Mali <saurabhmali2000@gmail.com>
2026-03-03 18:57:15 +0530

    Create 03FuncReturn.go

---
+++ 03FuncReturn.go
@@ -0,0 +1,20 @@
+// Function Return in GO
+
+//NOTE: When you return something from go func, 
+//You are supposed to mention the dtype of the 
+//return variable in the function declaration.
+
+//import
+package main
+import "fmt"
+
+func ReturnVariable(x int, y int) int {   // Note here
+    sum := 0
+    sum = x + y
+    return sum
+}
+
+func main(){
+    op := ReturnVariable(10,12)
+    fmt.Println(op)
+}
