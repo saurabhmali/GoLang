d3c3f072e646a3753726928b293edc26e849ab99
Saurabh Mali <saurabhmali2000@gmail.com>
2026-03-03 18:40:28 +0530

    Create FibonacciSeq.go

---
+++ FibonacciSeq.go
@@ -0,0 +1,16 @@
+//Print Fibonacci sequence
+package main
+import "fmt"
+
+func FibonacciSeq(){
+    a := 0                     
+    b := 1
+    for i:=1;i<10;i++ {
+        fmt.Print(a)
+        a , b = b, a + b
+    }
+}
+// Same no difference
+func main(){
+    FibonacciSeq()
+}
