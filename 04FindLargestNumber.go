93f067d8554333aa18dfe5f421de355670b23954
Saurabh Mali <saurabhmali2000@gmail.com>
2026-03-04 23:39:43 +0530

    Update 04FindLargestNumber.go

--- 04FindLargestNumber.go
+++ 04FindLargestNumber.go
@@ -1,1 +1,23 @@
 
+// Find Largest Number/////////////WRONG CODE
+
+package main
+import "fmt"
+
+func LargestNumber(arr []int, lnum int){
+    
+    for i:=0; i < len(arr); i++ {
+        if arr[i] > lnum {
+            lnum := arr[i]
+        }
+    }
+    
+}
+
+
+func main(){
+    arr1 := []int{99, 22, 44, 86, 10, 32}
+    lnum := arr1[0]
+    maxnum := LargestNumber(arr1,lnum)
+    fmt.Println(maxnum)
+}
